34f656d0baf1978b547003f465422993d6399b65
Sullivan677 <decarli.sulli@gmail.com>
2020-12-14 15:40:32 +0100

    Initial Commit

---
+++ MapLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapLocation
+//
+//  Created by Sullivan De carli on 14/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapLocation/MapLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocationApp.swift
+//  MapLocation
+//
+//  Created by Sullivan De carli on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MapLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
